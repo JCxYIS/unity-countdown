c7df3a59982e77a6d273d9992a846daa650ef12a
JCxYIS <jsn811@gmail.com>
2018-10-10 19:48:48 +0800

    init commit

---
+++ Unity_Countdown/Assets/Scripts/countdown.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdown: MonoBehaviour {
+	static public countdown instance;
+	public Transform stage;
+	//public Text countdownText;
+
+	List<DateTime> setTimer_destTime = new List<DateTime>
+	{
+		new DateTime(2018,12,18,09,15,00),
+		new DateTime(2019,01,25,09,15,00),
+		new DateTime(2019,07,01,00,00,00)
+	};
+	public List<Timer> setTimers;
+
+	[Header("RUNTIME")]
+	public List<timer> runTimers;
+
+	//-----------------------------------------------
+	
+	[Serializable]
+	public class Timer
+	{
+		public DateTime destTime;
+		public string name;
+		public Color color;
+	}
+
+	void Start() 
+	{
+		instance = this;
+		for(int i = 0; i < setTimers.Count; i++)
+		{
+			setTimers[i].destTime = setTimer_destTime[i];
+			runTimers.Add(InitTimer(setTimers[i]));
+		}
+	}
+	void Update() 
+	{
+		
+	}
+	
+	timer InitTimer(Timer timer)
+	{
+		GameObject g = Instantiate(Resources.Load<GameObject>("Prefabs/timer"), stage);
+		timer t = g.GetComponent<timer>();
+		t.isUsedAsClock = false;
+		t.myname = timer.name;
+		t.myDestTime = timer.destTime;
+		t.mycolor = timer.color;
+		return g.transform.GetComponent<timer>();
+	}
+
+}
---
+++ Unity_Countdown/Assets/Scripts/timer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+	[Tooltip("TRUE: show currTime")]
+	public bool isUsedAsClock;
+	public DateTime myDestTime;
+	public string myname;
+	public Text Name;
+	public Color mycolor;
+	public JumpingNumberTextComponent day;
+	public JumpingNumberTextComponent hr;
+	public JumpingNumberTextComponent min;
+	public JumpingNumberTextComponent sec;
+	public JumpingNumberTextComponent secff;
+	public Text timeformator;
+	public Transform mainDisplays;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		Name.text = myname;
+		Name.color = Color.white;
+		timeformator.color = Color.white;
+		for(int i = 0; i < mainDisplays.childCount; i++)
+			mainDisplays.GetChild(i).GetComponent<Text>().color = mycolor;
+		
+	}
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if(!isUsedAsClock)
+		{
+			TimeSpan mySpan = myDestTime.Subtract(DateTime.Now);
+			day.ChangeTo(mySpan.Days);
+			hr.ChangeTo(mySpan.Hours);
+			min.ChangeTo(mySpan.Minutes);
+			sec.ChangeTo(mySpan.Seconds);
+			secff.ChangeTo(mySpan.Milliseconds / 10);
+			//Debug.Log(mytime.Milliseconds);
+		}
+		else
+		{
+			day.ChangeTo(DateTime.Now.Month*100+DateTime.Now.Day);
+			hr.ChangeTo(DateTime.Now.Hour);
+			min.ChangeTo(DateTime.Now.Minute);
+			sec.ChangeTo(DateTime.Now.Second);
+		}
+	}
+}
